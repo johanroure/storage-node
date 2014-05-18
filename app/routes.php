2e4589df04d7ee769560a189ecd71bb17cafa79a
johan roure <j.roure@outlook.cl>
2014-05-18 19:13:18 -0400

    logger a superglobal

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,5 @@
 <?php
 return array(
-  array('type'=> 'POST', 'url'=> 'phpcli', 'callback'=> 'TestController@indice')
+  array('type'=> 'POST', 'url'=> 'phpcli', 'callback'=> 'TestController@indice'),
+  array('type'=> 'GET', 'url'=> 'storage-node/public/', 'callback'=> 'TestController@indice')
 );
--- public/index.php
+++ public/index.php
@@ -4,13 +4,30 @@ use \IPFilter\IPFilter;
 use \BasicAPP\Middleware;
 use \Symfony\Component\HttpFoundation\Request;
 use \Symfony\Component\HttpFoundation\Response;
-
+use \Katzgrau\KLogger\Logger;
+use \Psr\Log\LogLevel;
 /**
- * super globales request, response
+ * super globales request, response, logger
  */
-$GLOBALS['request'] = Request::createFromGlobals();
+/*
+LOG LEVELS
+LogLevel::EMERGENCY;
+LogLevel::ALERT;
+LogLevel::CRITICAL;
+LogLevel::ERROR;
+LogLevel::WARNING;
+LogLevel::NOTICE;
+LogLevel::INFO;
+LogLevel::DEBUG;
+*/
+
+
+$GLOBALS['request']  = Request::createFromGlobals();
 $GLOBALS['response'] = new Response();
-$GLOBALS['response']->headers->set('Content-Type', 'application/json');
+$GLOBALS['logger']   = new Logger('../app/logs', LogLevel::DEBUG);
+$GLOBALS['response']
+	->headers
+	->set('Content-Type', 'application/json');
 
 /**
  * verificar ip valida
@@ -23,4 +40,12 @@ $filter->WatchDog();
  * Es una request Valida
  */
 $app = new Middleware('../app/controllers/');
-$app->kickStart();
+$found = $app->kickStart();
+
+/**
+ * not found
+ */
+if($found === false) {
+    $GLOBALS['response']->setContent("{'status': 404}");
+    $GLOBALS['response']->send();
+}
